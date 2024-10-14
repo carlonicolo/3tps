d341391e681844e91f18e06dd04abe48bdc42cf9
Carlo <carlo.nicolo@hotmail.com>
2024-10-14 22:29:09 +0200

    hint

--- C/PrimaEsercitazione.c
+++ C/PrimaEsercitazione.c
@@ -16,5 +16,21 @@ int main()
     // i valori contenuti negli array dalla posizione
     // più piccola alla più grande e viceversa
 
+    // HINT
+    /**
+     * the sizeof operator returns the size of a type in bytes.
+     * 
+     * You learned from the Data Types chapter that an int type is usually 4 bytes, 
+     * so from the example above, 4 x 5 (4 bytes x 5 elements) = 20 bytes.
+     * Knowing the memory size of an array is great when you are working with
+     * larger programs that require good memory management.
+     * But when you just want to find out how many elements an array has,
+     * you can use the following formula (which divides the size of the array
+     * by the size of the first element in the array):
+     */
+    // int myNumbers[] = {10, 25, 50, 75, 100};
+    // int length = sizeof(myNumbers) / sizeof(myNumbers[0]);
+    // printf("%d", length);  // Prints 5
+
     return 0;
 }

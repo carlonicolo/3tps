dc8ca1e72342a7e71703ec0bba716ad7e03f3007
Carlo <carlo.nicolo@hotmail.com>
2024-10-14 22:03:35 +0200

    prima esercitazione

---
+++ C/PrimaEsercitazione.c
@@ -0,0 +1,20 @@
+/**
+ * Uso degli array e cicli
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    char greetings[] = "Hello World!";
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    printf("%s\n", greetings);
+    printf("%d\n", numbers[0]);
+
+    // TASK 1
+    // Ciclo for o while per stampare
+    // i valori contenuti negli array dalla posizione
+    // più piccola alla più grande e viceversa
+
+    return 0;
+}

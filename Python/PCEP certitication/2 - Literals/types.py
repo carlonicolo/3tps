0aa1fbdd6d394585df4c8a12d967f318ff12d3ba
Carlo <carlo.nicolo@hotmail.com>
2024-10-17 14:53:38 +0200

    fix

---
+++ Python/PCEP certitication/2 - Literals/types.py
@@ -0,0 +1,18 @@
+﻿# Octal
+print(0o123)
+
+# Hexadecimal
+print(0x123)
+
+# int
+print(5)
+
+# float
+print(4.5)
+
+# string (str)
+print("Hello")
+
+# Boolean True - False
+check = True
+print(check)

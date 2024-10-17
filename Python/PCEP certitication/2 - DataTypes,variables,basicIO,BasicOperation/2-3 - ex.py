5bd9aa2fed240fc4251d5831575404bc14d126f6
Carlo <carlo.nicolo@hotmail.com>
2024-10-17 15:23:42 +0200

    new labs and ex

---
+++ Python/PCEP certitication/2 - DataTypes,variables,basicIO,BasicOperation/2-3 - ex.py
@@ -0,0 +1,12 @@
+# Exercise 1
+# What is the output of the following snippet?
+print((2 ** 4), (2 * 4.), (2 * 4))
+
+
+# Exercise 2
+# What is the output of the following snippet?
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+
+# Exercise 3
+# What is the output of the following snippet?
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))
---
+++ Python/PCEP certitication/2 - DataTypes,variables,basicIO,BasicOperation/arithmetic.py
@@ -0,0 +1,22 @@
+﻿# Arithmetic operators: multiplication
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+
+print("")
+
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+print("")
+
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+
+# modulo
+print(14 % 4)

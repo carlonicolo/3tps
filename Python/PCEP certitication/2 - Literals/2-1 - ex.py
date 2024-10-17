2b98c8e6033c658ae360866ebd093150605c2de8
Carlo <carlo.nicolo@hotmail.com>
2024-10-17 14:58:47 +0200

    new exercice and lab

---
+++ Python/PCEP certitication/2 - Literals/2-1 - ex.py
@@ -0,0 +1,12 @@
+# Exercise 1
+#What types of literals are the following two examples?
+# "Hello ", "007"
+
+# Exercise 2
+# What types of literals are the following four examples?
+# "1.5", 2.0, 528, False
+
+
+# Exercise 3
+# What is the decimal value of the following binary number?
+# 1011

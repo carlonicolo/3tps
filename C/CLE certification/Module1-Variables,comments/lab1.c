f1f5bf8f8d01ca3ca691c5bc43077ffdaad1fc4b
Carlo <carlo.nicolo@hotmail.com>
2024-10-17 16:46:04 +0200

    first C lab exercice

---
+++ C/CLE certification/Module1-Variables,comments/lab1.c
@@ -0,0 +1,16 @@
+/**
+ * Level of difficulty
+ * Very Easy
+ * 
+ * Objectives
+ * Familiarize the student with:
+ * Writing a first program
+ * Printing on screen
+ * 
+ * The main function
+ * 
+ * Scenario
+ * Write your first program in the "C" language.
+ * Just print your name 3 times. Remember to include a return
+ * statement and make proper use of the main function.
+ */

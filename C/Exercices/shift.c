8db4eb73deccd964724de226f774c5299c0f5ee8
Carlo <carlo.nicolo@hotmail.com>
2024-10-23 22:25:52 +0200

    shift a destra conversione numeri binari

--- C/Exercices/shift.c
+++ C/Exercices/shift.c
@@ -5,9 +5,11 @@ int main(){
     int num = 32;
     for (int i = 31; i >= 0; i--) {
         int shift = (num >> i);
+        printf("%d i -> ", i);
         printf("Shift -> %d \n", shift);
         int bit = shift & 1;  // Shift the number and mask the last bit
         printf("bit -> %d\n", bit);
+        printf("\n");
     }
 
     return 0;

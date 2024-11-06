f16d855df1a5aaeb01237a9ae9a8b3862d687401
Carlo <carlo.nicolo@hotmail.com>
2024-11-06 22:28:56 +0100

    esercizio liste

---
+++ Python/Esercizi/Module 3 - list loops boolean/exList.py
@@ -0,0 +1,33 @@
+lst = []
+print(type(lst))
+
+# Add element
+lst1 = [2, 10, 4, 11, 5]
+lst1.append(9)
+
+# Copy list
+lst = lst1
+print(lst)
+print(lst1)
+
+# sort list
+lst.sort()
+print(lst)
+print(lst1)
+
+# len()
+print(len(lst))
+
+var = "Hello"
+print(len(var))
+print(var[-1])
+print(var[-2])
+print(var[-3])
+print(var[-4])
+print(var[-5])
+
+lst_word = [var[-1], var[-2], var[-3], var[-4], var[-5]]
+
+print(lst_word)
+
+# TASK 1 - recreate the original "Hello" var as list ['H','e','l','l','o']

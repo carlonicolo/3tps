dbdadbfd19f3ef5c678fd0d705a34d8e4b45f67a
K4rl1t0s <carlo.nicolo@hotmail.com>
2024-11-29 22:21:18 +0100

    New lab_char files

---
+++ C/CLE certification/Module2-Types and conditionals/lab7_char.c
@@ -0,0 +1,25 @@
+/*
+Level of difficulty
+Easy
+
+Objectives
+Familiarize the student with:
+
+Character types and values
+Using character types practically
+Printing on screen
+Scenario
+Write a small program which prints the differences between all ten digit characters (from '1' to '0') and zero ('0'). Print each one on a separate line. Your program must print the same result as the expected output.
+
+Expected output
+'1' - '0' is: 1
+'2' - '0' is: 2
+'3' - '0' is: 3
+'4' - '0' is: 4
+'5' - '0' is: 5
+'6' - '0' is: 6
+'7' - '0' is: 7
+'8' - '0' is: 8
+'9' - '0' is: 9
+'0' - '0' is: 0
+*/

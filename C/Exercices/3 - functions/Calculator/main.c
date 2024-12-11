b6c737d4bd9b57c6e44094ba9a9d5774786fa894
Carlo <carlo.nicolo@hotmail.com>
2024-12-11 23:10:24 +0100

    sub function

--- C/Exercices/3 - functions/Calculator/main.c
+++ C/Exercices/3 - functions/Calculator/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "sum.h"
+#include "sub.h"
 
 int main() {
     int param1 = 10, param2 = 5;
+    int res_sub;
 
     printf("Void function -> ");
     sum(param1, param2);
@@ -10,6 +12,12 @@ int main() {
 
     int res = add(param1, param2);
     printf("%d + %d = %d\n", param1, param2, res);
+
+    printf("\nSub function -> ");
+    
+    res_sub = sub(param1, param2);
+    printf("%d\n", res_sub);
+    res_sub = sub(param2, param1);
 
     return 0;
 }

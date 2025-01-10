33752a96c9cc22ce7a74792402062b371a53c31b
Carlo <carlo.nicolo@hotmail.com>
2025-01-10 22:43:41 +0100

    lab4 - Applicazione noleggio auto

---
+++ C/Exercices/Strutture/lab4.c
@@ -0,0 +1,16 @@
+/**
+ * @file lab4.c
+ * @author Carlo Nicolò (you@domain.com)
+ * @brief Simple rent car application
+ * @version 0.1
+ * @date 10/01/2025
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    return 0;
+}

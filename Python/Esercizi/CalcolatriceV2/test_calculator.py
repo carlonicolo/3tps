414d9b895df8e75d36f2a018e91893126abf91eb
K4rl1t0s <carlo.nicolo@hotmail.com>
2025-01-17 22:16:35 +0100

    Calcolatrice

---
+++ Python/Esercizi/CalcolatriceV2/test_calculator.py
@@ -0,0 +1,6 @@
+import calculator as calc
+import pytest
+
+
+def test_add():
+    assert calc.add(2,4)
---
+++ Python/PytestInstall.md
@@ -0,0 +1,5 @@
+# How to install Pytest library
+
+```bash
+sudo apt install python3-pytest
+```  

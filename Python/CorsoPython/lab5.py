f6109d1b1dc0a9e48dae6aff2ae048c6757c3130
Carlo <carlo.nicolo@hotmail.com>
2025-05-26 21:34:59 +0200

    Introduzione alle stringhe

---
+++ Python/CorsoPython/lab5.py
@@ -0,0 +1,20 @@
+# Stringhe in Python
+a = "Hello%"
+b = "World"
+c = a+b
+special_word = "bicicletta"
+print(c)
+
+hello = ("Hello" == "Hello") 
+hello1 = ("hello" == "Hello")
+
+print(hello)
+print(hello1)
+
+print(len(a))
+print(len(b))
+
+print(special_word[4:7])
+print(special_word[0])
+print(special_word[1])
+print(special_word[2])

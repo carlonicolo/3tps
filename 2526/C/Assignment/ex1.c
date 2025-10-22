a6261afd38821830816b4f0c427b1966b1561b72
Carlo <carlo.nicolo@hotmail.com>
2025-10-22 22:13:35 +0200

    First assignment

--- 2526/C/Assignment/ex1.c
+++ 2526/C/Assignment/ex1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-int main() {
-  
+int main()
+{
+
   int anni;
-  
+
   printf("Quanti anni hai? \n");
-  scanf("%d",&anni);
+  scanf("%d", &anni);
 
   /*
   Stampa:
@@ -14,7 +15,27 @@ int main() {
   17 a 25 ragazzo
   25 in su uomo
   */
-  
+
+  if (anni < 3)
+  {
+    printf("Sei un neonato");
+  }
+  else if (anni >= 3 && anni <= 12)
+  {
+    printf("Sei un bambino");
+  }
+  else if (anni > 12 && anni <= 17)
+  {
+    printf("Sei un adolescente");
+  }
+  else if (anni > 17 && anni <= 25)
+  {
+    printf("Sei un ragazzo");
+  }
+  else if (anni > 25)
+  {
+    printf("Sei un uomo");
+  }
 
   return 0;
 }

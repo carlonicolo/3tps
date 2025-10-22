31eb2ab340b316cae7c421ea37e14ec1d7dd37db
Carlo <carlo.nicolo@hotmail.com>
2025-10-22 22:20:59 +0200

    Assignment ex2.c

---
+++ 2526/C/Assignment/ex2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+
+    int tabellina;
+
+    printf("Quale tabellina vuoi che ti mostri?");
+    scanf("%d", &tabellina);
+
+    // TODO Utilizzare il ciclo while per stampare la tabellina del numero
+    //  inserito dall'utente fino a 10
+
+    // while(?){}
+
+    //TODO utilizzare il ciclo for per stampare la tabellina del numero inserito dall'utente
+    //for(?;?;?){}
+
+    return 0;
+}

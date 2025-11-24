d3ea89ff1df50cab68dd76deb4b96757937341ef
Carlo <carlo.nicolo@hotmail.com>
2025-11-24 23:10:36 +0100

    New assignment ex5 ed esercizio array in Flogorithm

--- 2526/C/Assignments/ex4.c
+++ 2526/C/Assignments/ex4.c
@@ -6,6 +6,10 @@ int main()
 {
     int numeri[DIM];
     int flag = 1;
+    int somma = 0;
+    float media;
+    int max;
+    int min;
 
     printf("Inserisci 5 numeri: \n");
     for (int cont = 0; cont < DIM; cont++)
@@ -13,8 +17,54 @@ int main()
         printf("Numero %d: ", flag);
         scanf("%d", &numeri[cont]);
         flag = flag + 1;
+        //somma = somma + numeri[cont];
         //flag++;
         //printf("\n");
+    }
+    //printf("\nLa somma dei valori dell'array è: %d", somma);
+    
+    printf("\n--- RISULTATI ---");
+
+    // Valori array
+    printf("\nArray: ");
+    for(int i=0; i < DIM; i++){
+        printf("%d ", numeri[i]);
+    }
+
+    // Somma
+    int sum = 0;
+    for(int i=0; i < DIM; i++){
+        sum = sum + numeri[i];
+    }
+
+    // Media
+    printf("\nSomma: %d", sum);
+    media = ((float)sum)/5;
+    printf("\nMedia: %.2f", media);
+
+
+    // Max
+    for(int i=0; i < DIM; i++){
+        if(numeri[i] > max){
+            max = numeri[i];
+        }
+    }
+    printf("\nMassimo: %d", max);
+
+    // Min
+    for(int i=0; i < DIM; i++){
+        if(numeri[i] < min){
+            min = numeri[i];
+        }
+    }
+    printf("\nMinimo: %d", min);
+
+    // Numeri pari
+    printf("\nElementi pari ");
+    for(int i=0; i < DIM; i++){
+        if(numeri[i] % 2 == 0){
+            printf("%d ", numeri[i]);
+        }
     }
 
     return 0;

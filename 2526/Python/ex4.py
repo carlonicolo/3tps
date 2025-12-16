cdfde2559bfc4f1bbf5569dd2f261a26e2f4d847
Carlo <carlo.nicolo@hotmail.com>
2025-12-16 20:35:51 +0100

    fix

--- 2526/Python/ex4.py
+++ 2526/Python/ex4.py
@@ -1,16 +1,18 @@
-def somma(a, b):
+def somma(a: int, b: int) -> int:
     risultato = a + b
     return risultato
 
-def sum_lst(lst):
-    print(lst[0])
-    
 
-def menu():
+def sum_lst(lst: list) -> int:
+    print(lst[0])
+
+
+def menu() -> None:
     num1 = int(input("Inserisci il primo numero: "))
     num2 = int(input("Inserisci il secondo numero: "))
     lst = [30, 40, 50]
-    print("La somma è:", somma(num1,num2))
+    print("La somma è:", somma(num1, num2))
     print(sum_lst(lst))
+
 
 menu()

1ccaacc5633c79a9385e8b5ae9aaa54d57e21f7f
Carlo <carlo.nicolo@hotmail.com>
2025-12-17 23:52:28 +0100

    bool

--- 2526/Python/ex4.py
+++ 2526/Python/ex4.py
@@ -3,16 +3,30 @@ def somma(a: int, b: int) -> int:
     return risultato
 
 
-def sum_lst(lst: list) -> int:
-    print(lst[0])
+def first_num_lst(lst: list) -> int:
+    # print(lst[0])
+    return lst[0]
 
-
-def menu() -> None:
+def menu() -> int:
     num1 = int(input("Inserisci il primo numero: "))
     num2 = int(input("Inserisci il secondo numero: "))
-    lst = [30, 40, 50]
-    print("La somma è:", somma(num1, num2))
-    print(sum_lst(lst))
+    return num1, num2
+
+num1, num2 = menu()
+res_somma = somma(num1, num2)
+print(f'("La somma di {num1} + {num2} è :", {res_somma})')
+# print("La somma è:", somma(num1, num2))
+
+lst = [30, 40, 50]
+first_num = first_num_lst(lst)
+print("Il primo numero della lista è: ", first_num)
+
+isNum1Maggiore = False 
+isBlue = False
+
+if(num1 > num2):
+    isNum1Maggiore = True
 
 
-menu()
+isEven = (num1 % 2 == 0)
+print("E' pari? ", isEven)
